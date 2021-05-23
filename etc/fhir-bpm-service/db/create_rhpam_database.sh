d82c7b69dcc0c2ff75e5ecae23d328abfe4bc03a
jbride <jbride2001@yahoo.com>
2021-05-23 08:10:57 -0600

    fhir-bpm-service now starts in docker-compose environment

--- etc/fhir-bpm-service/db/create_rhpam_database.sh
+++ etc/fhir-bpm-service/db/create_rhpam_database.sh
@@ -2,7 +2,8 @@
 
 echo "creating RHPAM database"
 
-psql -d $POSTGRESQL_DATABASE < ${SCRIPT_DIR}/postgresql-jbpm-schema.sql
+# psql -d $POSTGRESQL_DATABASE < ${SCRIPT_DIR}/postgresql-jbpm-schema.sql
+psql -d $POSTGRESQL_DATABASE < ${SCRIPT_DIR}/postgresql-springboot-jbpm-schema.sql
 psql -d $POSTGRESQL_DATABASE < ${SCRIPT_DIR}/task_assigning_tables_postgresql.sql
 psql -d $POSTGRESQL_DATABASE < ${SCRIPT_DIR}/quartz_tables_postgres.sql
 psql -d $POSTGRESQL_DATABASE < ${SCRIPT_DIR}/postgresql-jbpm-lo-trigger-clob.sql
