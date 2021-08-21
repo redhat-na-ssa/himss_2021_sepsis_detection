29416b8dd86a806210f9c29e4077b80e4326ca04
jbride <jbride2001@yahoo.com>
2021-08-20 21:04:57 -0600

    smoke test completed in OCP

--- ansible/resources/sepsisdetection-ui/env.js
+++ ansible/resources/sepsisdetection-ui/env.js
@@ -6,7 +6,7 @@
   window._env.KIE_SERVER_USERID = "kieserver";
   window._env.KIE_SERVER_PASSWORD = "{{ sepsisdetection_rhpam_api_passwd }}";
   window._env.DM_CONTAINER_ALIAS = "";
-  window._env.PAM_CONTAINER_ALIAS = "{{ sepsisdetection_rhpam_deployment_name }}";
+  window._env.PAM_CONTAINER_ALIAS = "{{ sepsisdetection_rhpam_deployment_name }}-{{ sepsisdetection_rhpam_deployment_version }}";
   window._env.PROCESS_ID = "{{ sepsisdetection_rhpam_process_id }}";
   window._env.FHIR_SERVER_URL = "https://{{ fhir_server_application_hostname }}";
   window._env.PATIENT_VIEWER_URL = "{{ patient_viewer_url }}";
