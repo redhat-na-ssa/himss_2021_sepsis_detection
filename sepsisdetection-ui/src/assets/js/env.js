db663b2d42e8c9198acc9eb1b2ea7df27d29f263
jbride <jbride2001@yahoo.com>
2021-08-23 17:29:33 -0600

    changed primary business process to sepsisdetection.bpmn and improved documentation

--- sepsisdetection-ui/src/assets/js/env.js
+++ sepsisdetection-ui/src/assets/js/env.js
@@ -7,7 +7,7 @@
   window._env.KIE_SERVER_PASSWORD = 'kieserver';
   window._env.DM_CONTAINER_ALIAS = '';
   window._env.PAM_CONTAINER_ALIAS = 'sepsisdetection-kjar-1.0.0';
-  window._env.PROCESS_ID = 'highmediummitigation';
+  window._env.PROCESS_ID = 'sepsisdetection';
   window._env.FHIR_SERVER_URL = 'http://localhost:8080';
   window._env.PATIENT_VIEWER_URL = '';
   window._env.IS_OPENSHIFT = 'false';
