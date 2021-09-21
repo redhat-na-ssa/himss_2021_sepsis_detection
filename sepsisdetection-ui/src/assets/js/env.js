0596bb0aa6a8344ad2c6e4871c9be6836eeacf6c
jbride <jbride2001@yahoo.com>
2021-09-21 08:59:49 -0600

    OIDC_enabled_workflow_apps.adoc:  even more additions

--- sepsisdetection-ui/src/assets/js/env.js
+++ sepsisdetection-ui/src/assets/js/env.js
@@ -4,7 +4,7 @@
 
   window._env.KEYCLOAK_URL = "http://localhost:4080";
   window._env.SSO_REALM = "kieRealm";
-  window._env.SSO_CLIENT = "kie-server";
+  window._env.SSO_CLIENT = "sepsisdetection";
   window._env.KIE_SERVER_URL = "http://localhost:9080";
   window._env.KIE_SERVER_USERID = 'kieserver';
   window._env.KIE_SERVER_PASSWORD = 'kieserver';
