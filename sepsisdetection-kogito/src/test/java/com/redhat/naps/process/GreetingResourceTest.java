3d83871873bca5e696d2a3586715f3a57b1210f3
jbride <jbride2001@yahoo.com>
2021-09-21 17:39:07 -0600

    sepsisdetection-kogito:  first draft

---
+++ sepsisdetection-kogito/src/test/java/com/redhat/naps/process/GreetingResourceTest.java
@@ -0,0 +1,16 @@
+package com.redhat.naps.process;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+    }
+
+}
