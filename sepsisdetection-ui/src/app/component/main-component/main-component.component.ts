74d05e66ce6d03cdc7aeed74e5dd303541e1a7d0
David van Balen <dvanbale@redhat.com>
2021-10-28 14:09:55 -0400

    Requested UI changes and redirect to SSO login page when not logged in already

--- sepsisdetection-ui/src/app/component/main-component/main-component.component.ts
+++ sepsisdetection-ui/src/app/component/main-component/main-component.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { UserRole } from 'src/app/Models/UserRole';
-import { faUser,faCog,faDatabase, faSignInAlt, faSignOutAlt } from '@fortawesome/free-solid-svg-icons';
+import { faUser,faCog,faDatabase, faSignInAlt, faSignOutAlt, faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 import { BackendSettingsComponent } from 'src/app/component/Modals/BackendSettings/BackendSettings.component';
 import { CompletedProcessInstanceComponent } from 'src/app/component/Modals/completed-process-instance/completed-process-instance.component';
 import { KeycloakService } from 'keycloak-angular';
 import { KeycloakProfile } from 'keycloak-js';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
 
  
 @Component({
@@ -18,51 +19,39 @@ export class MainComponent implements OnInit {
 
   user : UserRole;
   userList : UserRole[];
+  displayRoles : string[];
+  userRoles : string[];
   selectedOption : string = "";
+  userName : string = "";
+  userRole : string = "";
+  i : number = 0;
   faUser = faUser;
   faCogs = faCog;
   faDatabase = faDatabase;
   faSignIn = faSignInAlt;
   faSignOut = faSignOutAlt;
+  faArrowCircleLeft = faArrowCircleLeft;
 
   public isLoggedIn = false;
   public isAdminUser = false;
   public userProfile: KeycloakProfile | null = null;
 
   constructor(private modalService: NgbModal, public readonly keycloak: KeycloakService) {
-    
-    this.userList = [
-      {
-        id : 1,
-        name : "Andrew Smith",
-        role : "Admin",
-        userid : "AndrewSmith",
-        password : "test123"
-      },
-      {
-        id : 2,
-        name : "John Stark",
-        role : "Doctor",
-        userid : "JohnStark",
-        password : "test123"
-      },
-      /* {
-        id : 3,
-        name : "Stacie Dorsey",
-        role : "Approver",
-        userid : "StacieDorsey",
-        password : "test123"
-      }  */
+    this.displayRoles = [
+      "admin",
+      "doctor",
+      "provider"
     ];
-
-    this.user = this.userList[0];
   }
 
  changeSelectedOption(option : UserRole)
   {
-      this.user = option;
-      this.selectedOption = option.name;
-  }
+    if (option.userid == "logout" ) {
+      this.logout();
+    }
+     
+     this.user = option;
+     this.selectedOption = option.name;    }
 
   public async ngOnInit() {
     this.isLoggedIn = await this.keycloak.isLoggedIn();
@@ -71,6 +60,45 @@ export class MainComponent implements OnInit {
 
     if (this.isLoggedIn) {
       this.userProfile = await this.keycloak.loadUserProfile();
+      console.log("NAME: "+(await this.keycloak.getUsername())+" "+(await this.keycloak.loadUserProfile()).lastName)
+
+      // Get display name for user
+      if(!this.userProfile.firstName || !this.userProfile.lastName) {
+        this.userName = this.userProfile.username;
+      } else {
+        this.userName = this.userProfile.firstName+" "+this.userProfile.lastName;
+      }
+
+      // Get display role for user (choose first assigned role that's also in display list)
+      this.userRoles = this.keycloak.getUserRoles();
+      this.userRole = this.userRoles[0];
+      for(this.i=0;this.i<this.userRoles.length;this.i++) {
+        if(this.displayRoles.indexOf(this.userRoles[this.i]) !== -1) {
+          this.userRole = this.userRoles[this.i];
+          break;
+        }
+      }
+
+      this.userList = [
+        {
+          id : 1,
+          name : this.userName,
+          role : "( "+this.userRole+" )",
+          userid : this.keycloak.getUsername(),
+          password : "faUser"
+        },
+        {
+          id : 2,
+          name : "Log Out",
+          role : "",
+          userid : "logout",
+          password : "faArrowCircleLeft"
+        }
+      ];
+   
+      this.user = this.userList[0];
+    } else {
+      this.login();
     }
   }
 
